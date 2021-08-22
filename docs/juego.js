6e35543e9f37bb32d08572c63d65a39c9ddd6e82
Alan López de Jesús <33105245+alanlopezdejesus@users.noreply.github.com>
2021-08-21 21:33:38 -0500

    Update juego.js

--- docs/juego.js
+++ docs/juego.js
@@ -2,14 +2,20 @@ var contexto = document.getElementById("LienzoJuego").getContext("2d")
 contexto.canvas.width = 300
 contexto.canvas.height = 530
 
+//Variables
 var FPS = 60
 var gravedad = 1.5
-
 var personaje = {
-  x:100,
+  x:50,
   y:150,
   w:50,
   h:50
+}
+
+var tuberias = Array ()
+tuberias[0] = {
+  x:contexto.canvas.width,
+  y:0
 }
 
 //Variables imagenes
@@ -41,8 +47,21 @@ function loop() {
   contexto.drawImage(suelo,0,contexto.canvas.height - suelo.height)
   //Personaje
   contexto.drawImage(pajaro,personaje.x,personaje.y)
-    //Tuberias
-  
+  //Tuberias
+  for(i = 0; i < tuberias.length; i++){
+    var constante = tuberiaarriba.height + 80
+    contexto.drawImage(tuberiaarriba,tuberias[i].x,tuberias[i].y)
+    contexto.drawImage(tuberiaabajo,tuberias[i].x,tuberias[i].y + constante)
+    tuberias[i].x--
+    
+    if(tuberias[i].x == 150 ){
+      tuberias.push({
+        x:contexto.canvas.width,
+        y: Math.floor(Math.random()*tuberiaarriba.height) - tuberiaarriba.height
+      })
+    }
+  }
+  //Condiciones
   personaje.y += gravedad
 }
 
