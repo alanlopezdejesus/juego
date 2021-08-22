4105abdcd4e42527edbff87e165c2062d330743f
Alan López de Jesús <33105245+alanlopezdejesus@users.noreply.github.com>
2021-08-21 21:08:19 -0500

    Update juego.js

--- docs/juego.js
+++ docs/juego.js
@@ -1,6 +1,6 @@
 var contexto = document.getElementById("LienzoJuego").getContext("2d")
 contexto.canvas.width = 300
-contexto.canvas.height = 700
+contexto.canvas.height = 530
 
 var FPS = 60
 var gravedad = 1.5
@@ -12,6 +12,22 @@ var personaje = {
   h:50
 }
 
+//Variables imagenes
+var pajaro = new Image()
+pajaro.src="imagenes/bird.png"
+
+var fondo = new Image()
+fondo.src="imagenes/background.png"
+
+var tuberiaarriba = new Image()
+tuberiaarriba.src="imagenes/tuberiaNorte.png"
+
+var tuberiaabajo = new Image()
+tuberiaabajo.src="imagenes/tuberiaSur.png"
+
+var suelo = new Image()
+suelo.src="imagenes/suelo.png"
+
 //Control
 function presionar(){
   personaje.y -=35
@@ -20,9 +36,13 @@ function presionar(){
 setInterval(loop,1000/FPS)
 
 function loop() {
-  contexto.clearRect(0,0,300,700)
-  contexto.fillStyle="rgba(100,0,0,1)"
-  contexto.fillRect(personaje.x,personaje.y,personaje.w,personaje.h)
+  contexto.clearRect(0,0,300,530)
+  //Fonfo
+  contexto.drawImage(fondo,0,0)
+  contexto.drawImage(suelo,0,contexto.canvas.height - suelo.height)
+  //Personaje
+  contexto.drawImage(pajaro,personaje.x,personaje.y)
+    //Tuberias
   
   personaje.y += gravedad
 }
