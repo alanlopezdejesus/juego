e4fb4e59168f84287da7c4a98dd7ccaa85e10647
Alan López de Jesús <33105245+alanlopezdejesus@users.noreply.github.com>
2021-08-21 20:45:05 -0500

    Update juego.js

--- docs/juego.js
+++ docs/juego.js
@@ -1,1 +1,17 @@
-var contexto = document.getElementById()
+var contexto = document.getElementById("LienzoJuego").getContext("2d")
+context.canva.width = 300
+context.canva.height = 700
+
+var FPS = 60
+
+var personaje = {
+  x:100,
+  y:150
+}
+
+SetInterval(loop,1000/FPS)
+
+function loop{
+  contexto.fillStyle="rgba(100,0,0,1)"
+  contexto.fillRect(personaje.x,personaje.y,personaje.w,personaje.h)
+}
