5bc7174b0a62fa40c666c09666d59b8e902c2ac6
Alan López de Jesús <33105245+alanlopezdejesus@users.noreply.github.com>
2021-08-21 20:54:29 -0500

    Update juego.js

--- docs/juego.js
+++ docs/juego.js
@@ -3,6 +3,7 @@ contexto.canvas.width = 300
 contexto.canvas.height = 700
 
 var FPS = 60
+var gravedad = 1.5
 
 var personaje = {
   x:100,
@@ -11,9 +12,17 @@ var personaje = {
   h:50
 }
 
+//Control
+function keyDown(){
+  personaje.y -=25
+}
+
 setInterval(loop,1000/FPS)
 
 function loop() {
+  contexto.clearRect(0,0,300,700)
   contexto.fillStyle="rgba(100,0,0,1)"
   contexto.fillRect(personaje.x,personaje.y,personaje.w,personaje.h)
+  
+  personaje.y += gravedad
 }
