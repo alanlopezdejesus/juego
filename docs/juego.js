f9c15da8d77d7e54a5e89b5ea8db7896ac9a78bd
Alan López de Jesús <33105245+alanlopezdejesus@users.noreply.github.com>
2021-08-21 20:48:44 -0500

    Update juego.js

--- docs/juego.js
+++ docs/juego.js
@@ -6,12 +6,14 @@ var FPS = 60
 
 var personaje = {
   x:100,
-  y:150
+  y:150,
+  w:50,
+  h:50
 }
 
 SetInterval(loop,1000/FPS)
 
-function loop{
+function loop() {
   contexto.fillStyle="rgba(100,0,0,1)"
   contexto.fillRect(personaje.x,personaje.y,personaje.w,personaje.h)
 }
